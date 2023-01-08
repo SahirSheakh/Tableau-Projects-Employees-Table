85ba82fb3762230892c97cb430143c9f749dd13b
Sahir Sheakh <121377597+SahirSheakh@users.noreply.github.com>
2023-01-08 12:49:45 +1100

    Update ProjectTask_2_Script.sql

--- ProjectTask_2_Script.sql
+++ ProjectTask_2_Script.sql
@@ -1,3 +1,6 @@
+
+#Compare the number of male managers to the number of female managers from different departments for each year, starting from 1990.
+
 use employees_mod;
 
 select * from t_departments;
